511c0fe6461563ffc76b4c523b0b2a7f5fd2b611
yur <0511youli@gmail.com>
2025-01-28 03:11:12 +0900

    js, css 파일 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,46 +6,75 @@ import './Home.css';
 // React.Component 클래스의 기능을 추가한 Home 클래스
 class Home extends React.Component {
   state = {
-    isLoading : true,
-    movies : [],
+    isLoading: true,
+    movies: [],
+    showScrollButton: false, // 스크롤 버튼 표시 여부
   };
 
   getMovies = async () => {
     const {
-      data : {
-        data : { movies },
+      data: {
+        data: { movies },
       },
     } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    this.setState({ movies, isLoading : false });
+    this.setState({ movies, isLoading: false });
   };
 
   componentDidMount() {
     this.getMovies();
+    window.addEventListener("scroll", this.handleScroll); // 스크롤 이벤트 추가
   }
 
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll); // 스크롤 이벤트 제거
+  }
+
+  handleScroll = () => {
+    const showScrollButton = window.scrollY > 100; // 스크롤 위치가 100px 이상일 때 버튼 표시
+    this.setState({ showScrollButton });
+  };
+
+  scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // 부드럽게 스크롤
+    });
+  };
+
   render() {
-    const { isLoading, movies } = this.state;
+    const { isLoading, movies, showScrollButton } = this.state;
     return (
       <section className="container">
         {isLoading ? (
           <div className="loader">
-            <span className="loader__text">Loading...</span>
+            <div className="loader__dots">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
           </div>
-         ) : (
+        ) : (
           <div className="movies">
             {movies.map(movie => (
-             <Movie 
+              <Movie
                 key={movie.id}
                 id={movie.id}
                 year={movie.year}
-               title={movie.title}
-               summary={movie.summary}
-               poster={movie.medium_cover_image}
-               genres={movie.genres}
-             />
-           ))}
-         </div>
-        )} 
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+
+        {/* 최상단 이동 버튼 */}
+        {showScrollButton && (
+          <button className="scrollToTop" onClick={this.scrollToTop}>
+            ▲
+          </button>
+        )}
       </section>
     );
   }
